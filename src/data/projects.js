a98522f593e8586dcc5b780525560bfcf015207f
Loran Hendricks <lkh156@gmail.com>
2025-05-06 18:57:34 -0700

    added AI Legal Research Agent Project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,11 +2,11 @@ import UnderConstruction from '../assets/UnderConstruction.png';
 
 const projects = [
     {
-        title: 'Project One',
-        image: require('../assets/UnderConstruction.png'),
-        blurb: 'A quick overview of Project One.',
-        liveLink: 'https://',
-        repoLink: 'https://github.com/lkh156/',
+        title: 'AI Legal Research Agent',
+        image: require('../assets/ai-legal-research-agent.png'),
+        blurb: 'AI Legal Research Agent Upload legal complaints in PDF or DOCX format and receive the top 5 most relevant case law summaries — automatically analyzed and structured in IRAC format using AI. Built for law students, legal professionals, and legaltech enthusiasts.',
+        liveLink: 'https://lkh156.github.io/ai-legal-research-agent/',
+        repoLink: 'https://github.com/lkh156/ai-legal-research-agent',
     },
 
     {
